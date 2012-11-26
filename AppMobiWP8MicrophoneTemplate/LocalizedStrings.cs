73a1659be256e6c6f854a2bfa90cd426d60ce919
ryanfiorini <ryanwfiorini@gmail.com>
2012-11-26 09:57:29 -0500

    keeping up

---
+++ AppMobiWP8MicrophoneTemplate/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AppMobiWP8MicrophoneTemplate.Resources;
+
+namespace AppMobiWP8MicrophoneTemplate
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
